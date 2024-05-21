89c3147d66b5d6824efa63c377c50f71ca223a56
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-21 22:12:07 +0200

    feat: show all the dynamic information inside the cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,9 @@
                 <div class="col-3 my-3">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title">Title</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Original Title</h6>
-                            <p class="card-text">Voto: [voto]</p>
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Original Title: {{ $movie->original_title }}</h6>
+                            <p class="card-text">Voto: {{ $movie->vote }}</p>
                         </div>
                     </div>
                 </div>
